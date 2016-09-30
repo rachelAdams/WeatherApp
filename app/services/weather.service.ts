3addfdbebc0db7fdb5ae95a40ad0f5f74e5b0932
rachelAdams <rachel.adams1012@gmail.com>
2016-09-30 11:31:17 -0700

    TODO reminders for service

--- app/services/weather.service.ts
+++ app/services/weather.service.ts
@@ -10,7 +10,8 @@ export class WeatherService {
     constructor(private http: Http, private jsonp: Jsonp) { }
 
     getCurrentForecast(currentLat: any, currentLong: any) {
-        // get the current forecast for latitude and longitude
+        // get the current forecast for latitude and longitude 
+        // TODO: don't hardcode URL here
         var weatherUrl = "https://api.darksky.net/forecast/cf9cd81459a4017710b133ffa3d1bcf8/" + currentLat + "," + currentLong + "?callback=JSONP_CALLBACK";
         return this.jsonp
             .get(weatherUrl)
@@ -20,6 +21,7 @@ export class WeatherService {
 
     getLatLongFromZipCode(zipCode: string) {
         // get the latitude and longitude for a zip code
+        // TODO: don't hardcode URL here
         var zipCodeUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + zipCode + "&key=AIzaSyCE3MMVrk7VNqEZ-KKxeP-RSOxpwDvrQFs";
         return this.http
             .get(zipCodeUrl)
@@ -28,7 +30,8 @@ export class WeatherService {
     }
 
     getZipCodeFromLatLong(currentLat: any, currentLong: any) {
-         // get the zip code for a latitude and longitude
+        // get the zip code for a latitude and longitude
+        // TODO: don't hardcode URL here
         var latLongUrl = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + currentLat + "," + currentLong + "&result_type=postal_code&key=AIzaSyCE3MMVrk7VNqEZ-KKxeP-RSOxpwDvrQFs";
         return this.http
             .get(latLongUrl)
